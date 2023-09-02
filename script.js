4dd6c47e8abaf1feb9dee0e8ffeb704f2bd7d858
saad <69530708+Saad042@users.noreply.github.com>
2023-09-02 20:15:54 +0500

    add hover effect

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ function addRows(parentDiv) {
     rowDiv.classList.add("flex-container");
     rowDiv.classList.add("flex");
     for (let j = 0; j < 16; j++) {
-      const columnDiv = document.createElement('div');
+      const columnDiv = document.createElement("div");
       columnDiv.classList.add("grid-element");
       columnDiv.classList.add("flex");
+      addHoverEvents(columnDiv);
       rowDiv.appendChild(columnDiv);
     }
     parentDiv.appendChild(rowDiv);
   }
 }
 
-
+function addHoverEvents(div) {
+  div.addEventListener("mouseover", () => {
+    div.classList.add("active-grid-element");
+  });
+  div.addEventListener("mouseout", () => {
+    div.classList.remove("active-grid-element");
+  });
+}
 
 const parentDiv = document.getElementById("parentDiv");
 addRows(parentDiv);
